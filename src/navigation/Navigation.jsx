8877f684922c2130abed38c8f94c0065736511c9
Akshat Kumar <akshatkumar4433@gmail.com>
2023-04-17 12:48:29 -0400

    fixed-positions

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -5,20 +5,20 @@ import EmailIcon from '@mui/icons-material/Email';
 function Navigation() {
   return (
     <div className='flex flex-row  justify-around'>
-        <div className='grow border text-center'>
+        <div className='grow  text-center'>
                 <span className='text-2xl'>
                     Akshat Kumar
                 </span>
         </div>
-        <div className='text-4xl grow w-20 border' >
-                <ReceiptLongIcon
-                    fontSize = 'inherit'
-                />
-        </div>
-        <div className='text-4xl grow w-20 border'>
-                <EmailIcon
-                   fontSize = 'inherit'
-                />
+        <div className='text-4xl grow  flex  gap-7 justify-center'>
+                        <ReceiptLongIcon
+                            fontSize = 'inherit'
+                        />
+                        <EmailIcon
+                            fontSize = 'inherit'
+                        />
+
+                
         </div>
         
     </div>
