1b404839bfb4126e82ea575d7bc634f9337e4ff1
Akshat Kumar <akshatkumar4433@gmail.com>
2023-04-17 12:06:38 -0400

    font-fixed

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-
+import EmailIcon from '@mui/icons-material/Email';
 
 function Navigation() {
   return (
     <div className='flex flex-row gap-2'>
-        <div className='grow border text-lg'>
+        <div className='grow  text-lg'>
             <span className='text-lg'>
                 Akshat Kumar
             </span>
         </div>
-        <div className='border' >
-            <span className='text-base'>
-                <ReceiptLongIcon/>
-            </span>
+        <div className='text-4xl' >
+                <ReceiptLongIcon
+                    fontSize = 'inherit'
+                />
         </div>
-        <div className='border'>
-            <span className='text-base'>
-                Contact
-            </span>
+        <div className='text-4xl'>
+                <EmailIcon
+                   fontSize = 'inherit'
+                />
         </div>
+        
     </div>
   )
 }
