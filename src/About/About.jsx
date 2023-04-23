bcd43b0384e0b80d8460a8f9d199e4ba9539ce97
Akshat Kumar <akshatkumar4433@gmail.com>
2023-04-23 03:20:45 -0400

    about layout

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='border h-screen'>
-        <div>
-            <p>
+    <div className='border h-screen relative'>
+        <div className='border max-w-md absolute left-20 right-20 top-40'>
+            <p className='tablet:text-xl desktop:text-2xl'>
                 I'm Akshat Kumar computer programmer and web developer.
                 I am always looking for new challenges and opportunites to learn and grow.
-                Have fun and check out my Projects
+                Have fun and check out my Projects.
             </p>
         </div>
     </div>
