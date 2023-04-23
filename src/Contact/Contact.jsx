4cdb2d0f877bc6fba9e7b066b06d37fafeab4c84
Akshat Kumar <akshatkumar4433@gmail.com>
2023-04-23 09:58:26 -0400

    text-box

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='flex flex-col border-2 
+    <div className='flex flex-col  
                     content-center gap-2 
                     tablet:mx-10
                     tablet:mt-10
                     '>
-      <div className='border'>
-        <label className = 'block text-xl'for = 'email'  >Email:</label>
-        <input className='border-2' type= 'text' name = 'email' />
+      <div className='border p-10'>
+        <label className = 'block text-2xl'for = 'email'  >Email:</label>
+        <input className='border-2 w-full text-lg' type= 'text' name = 'email' />
       </div>
-      <div className='border'>
-      <label className = 'block text-xl' for = 'message' >Message:</label>
-        <textarea  className = 'border-2' type= 'text' name = 'message' />
+      <div className='border mt-2 pr-10 pl-10 '>
+      <label className = 'block text-2xl' for = 'message' >Message:</label>
+        <textarea  className = 'border-2 w-full text-lg' rows='5' type= 'text' name = 'message' />
       </div>
        <div className='border'>
 
