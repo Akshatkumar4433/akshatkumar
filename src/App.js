4e13ba5a2f00628e0d14a14f6c98baeb8eed95bb
Akshat Kumar <akshatkumar4433@gmail.com>
2023-04-23 03:44:17 -0400

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Navigation from './navigation/Navigation';
 import About from './About/About';
+import Contact from './Contact/Contact';
+import Projects from './Projects/Projects';
+import NoPage from './NoPage/NoPage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   
   return (
     <>
-      <Navigation
-      />
-      <About/>
+      <Navigation/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element = {<About/>}/>
+          <Route path = 'projects' element = {<Projects/>} />
+          <Route path = 'contact' element = {<Contact/>} />
+          <Route path = '*' element = {<NoPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
