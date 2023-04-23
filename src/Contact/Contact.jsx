f0faab531e4ab25a39a604656e58b845d1bac6ef
Akshat Kumar <akshatkumar4433@gmail.com>
2023-04-23 09:45:12 -0400

    fixed text

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='flex flex-col border-2 
+                    content-center gap-2 
+                    tablet:mx-10
+                    tablet:mt-10
+                    '>
+      <div className='border'>
+        <label className = 'block text-xl'for = 'email'  >Email:</label>
+        <input className='border-2' type= 'text' name = 'email' />
+      </div>
+      <div className='border'>
+      <label className = 'block text-xl' for = 'message' >Message:</label>
+        <textarea  className = 'border-2' type= 'text' name = 'message' />
+      </div>
+       <div className='border'>
+
+       <button>Send</button>
+       </div>
+      
+    </div>
   )
 }
 
